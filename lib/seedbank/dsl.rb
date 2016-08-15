f7cc0ea19e642e6f6451dac39a16ae2c8ccf41a9
James McCarthy <james@thisishatch.co.uk>
2016-08-15 08:01:02 +0700

    Fix merge mess.

--- lib/seedbank/dsl.rb
+++ lib/seedbank/dsl.rb
@@ -1,67 +1,67 @@
 module Seedbank
   module DSL
     refine Object do
-    def override_seed_task(*args)
-      task_name, arg_names, deps = Rake.application.resolve_args(args)
-      seed_task = Rake::Task.task_defined?(task_name) ? Rake::Task[task_name].clear : Rake::Task.define_task(task_name)
-      add_comment_to(seed_task, Rake.application.last_description)
-      add_environment_dependency(seed_task)
-      seed_task.enhance deps
-    end
-
-    def seed_task_from_file(seed_file)
-      scopes  = scope_from_seed_file(seed_file)
-      fq_name = scopes.push(File.basename(seed_file, '.seeds.rb')).join(':')
-
-      define_seed_task(seed_file, fq_name)
-    end
-
-    def glob_seed_files_matching(*args, &block)
-      Dir.glob(File.join(seeds_root, *args), &block)
-    end
-
-    def runner
-      @_seedbank_runner ||= Seedbank::Runner.new
-    end
-
-    def define_seed_task(seed_file, *args)
-      task = Rake::Task.define_task(*args) do |seed_task|
-        runner.evaluate(seed_task, seed_file) if File.exist?(seed_file)
+      def override_seed_task(*args)
+        task_name, arg_names, deps = Rake.application.resolve_args(args)
+        seed_task = Rake::Task.task_defined?(task_name) ? Rake::Task[task_name].clear : Rake::Task.define_task(task_name)
+        add_comment_to(seed_task, Rake.application.last_description)
+        add_environment_dependency(seed_task)
+        seed_task.enhance deps
       end
 
-      task.add_description "Load the seed data from #{seed_file}"
-      add_environment_dependency(task)
-      task.name
-    end
+      def seed_task_from_file(seed_file)
+        scopes  = scope_from_seed_file(seed_file)
+        fq_name = scopes.push(File.basename(seed_file, '.seeds.rb')).join(':')
 
-    def add_environment_dependency(task)
-      if Rake::Task.task_defined?('db:abort_if_pending_migrations')
-        task.enhance(['db:abort_if_pending_migrations'])
-      elsif Rake::Task.task_defined?(':environment')
-        task.enhance([':environment'])
+        define_seed_task(seed_file, fq_name)
       end
-    end
 
-    def scope_from_seed_file(seed_file)
-      dirname = Pathname.new(seed_file).dirname
-      return [] if dirname == seeds_root
-      relative = dirname.relative_path_from(seeds_root)
-      relative.to_s.split(File::Separator)
-    end
+      def glob_seed_files_matching(*args, &block)
+        Dir.glob(File.join(seeds_root, *args), &block)
+      end
 
-    def seeds_root
-      Pathname.new Seedbank.seeds_root
-    end
+      def runner
+        @_seedbank_runner ||= Seedbank::Runner.new
+      end
 
-    private
+      def define_seed_task(seed_file, *args)
+        task = Rake::Task.define_task(*args) do |seed_task|
+          runner.evaluate(seed_task, seed_file) if File.exist?(seed_file)
+        end
 
-    def add_comment_to(seed_task, comment)
-      if seed_task.respond_to?(:clear_comments)
-        seed_task.comment = comment
-      else
-        seed_task.send :instance_variable_set, '@full_comment', comment
+        task.add_description "Load the seed data from #{seed_file}"
+        add_environment_dependency(task)
+        task.name
+      end
+
+      def add_environment_dependency(task)
+        if Rake::Task.task_defined?('db:abort_if_pending_migrations')
+          task.enhance(['db:abort_if_pending_migrations'])
+        elsif Rake::Task.task_defined?(':environment')
+          task.enhance([':environment'])
+        end
+      end
+
+      def scope_from_seed_file(seed_file)
+        dirname = Pathname.new(seed_file).dirname
+        return [] if dirname == seeds_root
+        relative = dirname.relative_path_from(seeds_root)
+        relative.to_s.split(File::Separator)
+      end
+
+      def seeds_root
+        Pathname.new Seedbank.seeds_root
+      end
+
+      private
+
+      def add_comment_to(seed_task, comment)
+        if seed_task.respond_to?(:clear_comments)
+          seed_task.comment = comment
+        else
+          seed_task.send :instance_variable_set, '@full_comment', comment
+        end
       end
     end
   end
-end
 end
--- test/lib/seedbank/dsl_test.rb
+++ test/lib/seedbank/dsl_test.rb
@@ -1,79 +1,78 @@
 require 'test_helper'
 
 describe Seedbank::DSL do
-  describe "scope_from_seed_file" do
-    it "is added to the namesapce" do
+  describe 'scope_from_seed_file' do
+    it 'is added to the namesapce' do
       ns = Rake.application.in_namespace(:seedy) { self }
 
       ns.must_respond_to :scope_from_seed_file
     end
 
-    describe "in an environment directory" do
+    describe 'in an environment directory' do
       let(:seed_file) { File.expand_path('development/users.seeds.rb', Seedbank.seeds_root) }
       let(:seed_namespace) { %w(development) }
 
       subject { Seedbank::DSL.scope_from_seed_file seed_file }
 
-      it "returns the enviroment scope" do
+      it 'returns the enviroment scope' do
         subject.must_equal seed_namespace
       end
     end
 
-    describe "in a nested directory" do
+    describe 'in a nested directory' do
       let(:seed_file) { File.expand_path('development/shared/accounts.seeds.rb', Seedbank.seeds_root) }
       let(:seed_namespace) { %w(development shared) }
 
       subject { Seedbank::DSL.scope_from_seed_file seed_file }
 
-      it "returns the nested scope" do
+      it 'returns the nested scope' do
         subject.must_equal seed_namespace
       end
     end
 
-    describe "in seeds root" do
+    describe 'in seeds root' do
       let(:seed_file) { File.expand_path('no_block.seeds.rb', Seedbank.seeds_root) }
 
       subject { Seedbank::DSL.scope_from_seed_file seed_file }
 
-      it "returns an array" do
+      it 'returns an array' do
         subject.must_be_instance_of Array
       end
 
-      it "must be empty" do
+      it 'must be empty' do
         subject.must_be_empty
       end
     end
   end
 
-  describe "seeds_root" do
+  describe 'seeds_root' do
     let(:seeds_root) { '/my/seeds/directory' }
 
     subject { Seedbank::DSL.send(:seeds_root) }
 
-    it "returns a Pathname" do
-      Seedbank.stub(:seeds_root, seeds_root) do
+    it 'returns a Pathname' do
+      Seedbank.stub(:seeds_root, seeds_root)
 
       subject.must_equal Pathname.new(seeds_root)
     end
   end
 
-  describe "glob_seed_files_matching" do
-
-    describe "with no namespace" do
+  describe 'glob_seed_files_matching' do
+    describe 'with no namespace' do
       let(:pattern) { '*.seeds.rb' }
 
-      it "returns all the files matching the pattern in seeds_root" do
+      it 'returns all the files matching the pattern in seeds_root' do
         expected_files = Dir.glob(File.join(Seedbank.seeds_root, pattern))
 
         Seedbank::DSL.glob_seed_files_matching(pattern).must_equal expected_files
       end
     end
 
-    describe "with a namespace" do
+    describe 'with a namespace' do
       let(:pattern) { '*.seeds.rb' }
       let(:namespace) { 'development' }
 
-      it "returns all the files matching the pattern in seeds_root" do
+      it 'returns all the files matching the pattern in seeds_root' do
         expected_files = Dir.glob(File.join(Seedbank.seeds_root, namespace, pattern))
 
         Seedbank::DSL.glob_seed_files_matching(namespace, pattern).must_equal expected_files
@@ -81,30 +80,30 @@ describe Seedbank::DSL do
     end
   end
 
-  describe "define_seed_task" do
+  describe 'define_seed_task' do
     let(:task_name) { 'scoped:my_seed' }
     let(:dependencies) { ['environment'] }
     let(:seed_file) { File.expand_path('development/users.seeds.rb', Seedbank.seeds_root) }
 
-    it "returns a fully qualified task name" do
+    it 'returns a fully qualified task name' do
       returned_name = Seedbank::DSL.define_seed_task(seed_file, task_name => dependencies)
 
       returned_name.must_equal task_name
     end
 
-    it "creates a Rake Task" do
+    it 'creates a Rake Task' do
       Seedbank::DSL.define_seed_task(seed_file, task_name => dependencies)
 
       Rake::Task[task_name].wont_be_nil
     end
 
-    it "sets Rake Task description" do
+    it 'sets Rake Task description' do
       Seedbank::DSL.define_seed_task(seed_file, task_name => dependencies)
 
       Rake::Task[task_name].comment.must_equal "Load the seed data from #{seed_file}"
     end
 
-    it "sets Rake Task action to the seed_file contents" do
+    it 'sets Rake Task action to the seed_file contents' do
       Seedbank::DSL.define_seed_task(seed_file, task_name => dependencies)
 
       FakeModel.expect :seed, true, ['development:users']
@@ -112,8 +111,8 @@ describe Seedbank::DSL do
       Rake::Task[task_name].invoke
     end
 
-    describe "when db:abort_if_pending_migrations exists" do
-      it "sets Rake Task dependencies" do
+    describe 'when db:abort_if_pending_migrations exists' do
+      it 'sets Rake Task dependencies' do
         Seedbank::DSL.define_seed_task(seed_file, task_name => dependencies)
         expected_dependencies = dependencies.map { |dependency| Rake::Task[dependency] }
         expected_dependencies << Rake::Task['db:abort_if_pending_migrations']
@@ -122,8 +121,8 @@ describe Seedbank::DSL do
       end
     end
 
-    describe "when db:abort_if_pending_migrations does not exist" do
-      it "sets Rake Task dependencies" do
+    describe 'when db:abort_if_pending_migrations does not exist' do
+      it 'sets Rake Task dependencies' do
         Rake.application.clear
         Rake::Task.define_task 'environment'
 
@@ -134,25 +133,25 @@ describe Seedbank::DSL do
     end
   end
 
-  describe "override_seed_task" do
-    describe "when no task exists to override" do
+  describe 'override_seed_task' do
+    describe 'when no task exists to override' do
       let(:name) { 'my_task' }
       let(:dependencies) { ['db:abort_if_pending_migrations', 'seedy:users'] }
 
-      it "creates a new task" do
+      it 'creates a new task' do
         Seedbank::DSL.override_seed_task(task_name => dependencies)
 
         Rake::Task[task_name].wont_be_nil
       end
 
-      it "applies the dependencies" do
+      it 'applies the dependencies' do
         expected_dependencies = dependencies.map { |dependency| Rake::Task[dependency] }
         Seedbank::DSL.override_seed_task(task_name => dependencies)
 
         Rake::Task[task_name].prerequisite_tasks.must_equal expected_dependencies
       end
 
-      it "applies the description" do
+      it 'applies the description' do
         description = 'Expected Description'
         Rake.application.last_description = description
 
